0e9c0760c78b8bc930817ede293ae364264e8dc1
MontereyLeeAnnJenica <leeannjenica5@gmail.com>
2022-03-14 09:02:25 +0800

    My Educational Background Added

--- git-python-monterey.py
+++ git-python-monterey.py
@@ -1,3 +1,6 @@
 print ("Lee-Ann Jenica R. Monterey")
 print ("Masaguisi, Santa Cruz Marinduque")
 print ("November 5, 2002")
+print ("Marinduque State College")
+print ("BS Information Technology")
+print ("2nd Year - Section: D")
